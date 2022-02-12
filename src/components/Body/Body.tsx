6165e9eb724c153b0f6234bf182842d92abf53a9
Anurag30112003 <anuragsharma011011@gmail.com>
2022-02-13 02:58:12 +0530

    starting

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Body = () => {
     return (
         <div className="body">
-            This is body
+            Hello There I am Anurag
         </div>
         
     );

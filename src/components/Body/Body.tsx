2b72c14fcf599281d1938cca751596514ab3ae56
Anurag30112003 <anuragsharma011011@gmail.com>
2022-02-17 16:42:33 +0530

    wardering around Created by co-pilot

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-
+import './Body.css';
 export const Body = () => {
-    return (
-        <div className="body">
-            Hello There I am Anurag
+  return (
+    <>
+        <div>
+            <h1> Hello There ! </h1>
+            <p>
+                I am a full stack developer with a passion for building web applications.
+                I have a strong background in front-end development and have worked on a wide range of projects.
+            </p>
+            <p>
+                I am a self-taught developer.
+                I have a passion for learning new things and I am always looking to improve my skills.
+            </p>
+            <p>
+                I am currently looking for a position in the software industry.
+                If you are interested in working with me, please get in touch.
+            </p>
+            <a href="mail.com "><img src="https://img.icons8.com/external-justicon-flat-justicon/50/000000/external-gmail-social-media-justicon-flat-justicon.png" alt='gmail '/></a>
+            
         </div>
-        
-    );
-}
+    </>
+  );
+};

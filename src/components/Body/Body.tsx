da830e85cc37504ce1966ee2a5a7824454dec8ba
Anurag30112003 <anuragsharma011011@gmail.com>
2022-02-19 00:56:36 +0530

    intro

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,24 +1,25 @@
-import './Body.css';
+import "./Body.css";
+import sitting from '../img/vector_siting.svg';
 export const Body = () => {
   return (
     <>
-        <div>
-            <h1> Hello There ! </h1>
-            <p>
-                I am a full stack developer with a passion for building web applications.
-                I have a strong background in front-end development and have worked on a wide range of projects.
-            </p>
-            <p>
-                I am a self-taught developer.
-                I have a passion for learning new things and I am always looking to improve my skills.
-            </p>
-            <p>
-                I am currently looking for a position in the software industry.
-                If you are interested in working with me, please get in touch.
-            </p>
-            <a href="mail.com "><img src="https://img.icons8.com/external-justicon-flat-justicon/50/000000/external-gmail-social-media-justicon-flat-justicon.png" alt='gmail '/></a>
-            
-        </div>
+      <div>
+        <h1> Hello There ! </h1>
+        <img  src={sitting} className="profileimg" alt="human" />
+        <p>
+          I am a full stack developer with a passion for building web
+          applications. I have a strong background in front-end development and
+          have worked on a wide range of projects.
+        </p>
+        <p>
+          I am a self-taught developer. I have a passion for learning new things
+          and I am always looking to improve my skills.
+        </p>
+        <p>
+          I am currently looking for a position in the software industry. If you
+          are interested in working with me, please get in <span className="touch">touch.</span>
+        </p>     
+      </div>
     </>
   );
 };
